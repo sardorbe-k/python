4282f231e6f20d3d983dba6df38796ec606719ea
Sardorbek <sardorbekhamidovv@gmail.com>
2025-09-12 17:49:13 +0500

    Update practice.py

--- lesson-1/homework/practice.py
+++ lesson-1/homework/practice.py
@@ -1,1 +1,24 @@
+# 1 Given a side of square. Find its perimeter and area.
+sideo_square = 12
+print('perimeter=',sideo_square*4,'area=', sideo_square**2)
+
+# 2 Given diameter of circle. Find its length.
+d = 15
+print('L=',d*3.14)
+
+# 3 Given two numbers a and b. Find their mean.
+a = 24
+b = 34
+mean = (a + b) / 2
+print('Mean=' , mean)
+
+# 4 Given two numbers a and b. Find their sum, product and square of each number.
+first_num =  5 
+second_num = 10
+
+addition = first_num + second_num
+multiplication = first_num * second_num
+
+print('sum=', addition,'multiplication=', multiplication, first_num ** 2, second_num**2)
+
 
